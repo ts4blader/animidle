1dc04dfba8305f1745cd92738b913ccdc00427e6
ts4blader <ts4blader@gmail.com>
2021-10-24 20:01:05 +0700

    404 style

---
+++ pages/404.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import Header from "../components/Header";
+import Icon from "../components/Icon";
+
+export default function NotFound() {
+  return (
+    <div className="notfound-page">
+      <Header />
+      <div className="notfound-page__content container">
+        <h2 className="title">Huh? Lost your way?</h2>
+        <p className="subtitle">
+          Sorry, we can not find that page. You will find lots to explore on our
+          homepage
+        </p>
+        <div className="cta-btn">
+          <Link href="/">
+            <a>
+              <Icon src="caret.png" alt="caret" />
+              <p>Home</p>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
