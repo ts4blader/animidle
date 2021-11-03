cab73447955b9719f071f9e4b17c41ef33c91a87
ts4blader <ts4blader@gmail.com>
2021-11-03 23:49:15 +0700

    slide coms

---
+++ components/Slide.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "./Card";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+export default function Slide({ content = [] }) {
+  return (
+    <Splide
+      className="slide"
+      options={{
+        rewind: true,
+        gap: "1rem",
+        perPage: 5,
+        perMove: 1,
+        autoplay: true,
+        interval: 10000,
+        pauseOnHover: true,
+        isNavigation: false,
+      }}
+    >
+      {content.map((item, index) => {
+        return (
+          <SplideSlide key={index}>
+            <Card content={item} />
+          </SplideSlide>
+        );
+      })}
+    </Splide>
+  );
+}
